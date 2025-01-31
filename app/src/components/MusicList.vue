240f84f4682ffbb3b7ce9db69eed425399465ae1
Jasong237 <jasonguo4635@gmail.com>
2025-01-31 13:46:17 -0500

    sdfdsf

--- app/src/components/MusicList.vue
+++ app/src/components/MusicList.vue
@@ -1,9 +1,15 @@
-<template>
-  <div>
-    <h1>The movies will go here ujyhtijhtiyjhityjhijtyhijytijijtgbritjgirjtgijtrigjtrijgji</h1>
-  </div>
-</template>
+<script setup>
+const albums = [{ name: 'Hey' }, { name: 'Yay' }]
+</script>
 
-<script setup></script>
+<template>
+  <main>
+    <!--     <LogIn message="hello world" :count="5"><h1>Hello Jason</h1></LogIn>
+    <AnimalCard v-for="animal in animals" :key="animal.name" :animal="animal" /> -->
+    <div>
+      <h1>The movies will go here ujyhtijhtiyjhityjhijtyhijytijijtgbritjgirjtgijtrigjtrijgji</h1>
+    </div>
+  </main>
+</template>
 
 <style scoped></style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,15 +1,10 @@
 <script setup>
-/* import LogIn from '../components/LoginTest.vue'
-import AnimalCard from '../components/AnimalCard.vue'
- */ import AlbumList from '../components/AlbumList.vue'
-/* const animals = [{ name: 'capybara' }, { name: 'bat' }] */
-const albums = [{ name: 'Hey' }, { name: 'Yay' }]
+import AlbumList from '../components/AlbumList.vue'
+const albums = [{ name: 'Album 1' }, { name: 'Album 2' }]
 </script>
 
 <template>
   <main>
-    <!--     <LogIn message="hello world" :count="5"><h1>Hello Jason</h1></LogIn>
-    <AnimalCard v-for="animal in animals" :key="animal.name" :animal="animal" /> -->
     <AlbumList v-for="album in albums" :key="album.name" :album="album" />
     <div>
       <h1 class="text-3xl font-bold underline">Hello</h1>
