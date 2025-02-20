b798fd0e7f24af6628713bd51533c3d1e3de0e49
Jasong237 <jasonguo4635@gmail.com>
2025-02-20 14:03:18 -0500

    egregre

--- app/src/components/TrueAlbums.vue
+++ app/src/components/TrueAlbums.vue
@@ -5,6 +5,18 @@
 <script>
 import { reactive, watch } from 'vue'
 
+import overlydedicated from '@/assets/albums/overlydedicated.jpg'
+import section80 from '@/assets/albums/section80.jpg'
+import gkmc from '@/assets/albums/gkmc.jpg'
+import gkmcd from '@/assets/albums/gkmcd.jpg'
+import tpab from '@/assets/albums/tpab.jpg'
+import uu from '@/assets/albums/ntitled.jpg'
+import damn from '@/assets/albums/damn.jpg'
+import bp from '@/assets/albums/bp.jpg'
+import mmatbs from '@/assets/albums/mmatbs.jpg'
+import gnx from '@/assets/albums/gnx.jpg'
+import nls from '@/assets/albums/nls.jpg'
+
 export function useAlbums() {
   const storedAlbums = JSON.parse(localStorage.getItem('albums')) || []
 
@@ -13,93 +25,93 @@ export function useAlbums() {
       ? storedAlbums.sort((a, b) => a.id - b.id)
       : [
           {
-            name: 'Overly Dedicated',
-            id: '1',
-            img: '@/assets/albums/overlydedicated.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Section.80',
-            id: '2',
-            img: '@/assets/albums/section80.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Good Kid, M.A.A.D City',
-            id: '3',
-            img: '@/assets/albums/gkmc.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Good Kid, M.A.A.D City (Deluxe)',
-            id: '4',
-            img: '\src\assets\albums\gkmcd.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'To Pimp a Butterfly',
-            id: '5',
-            img: '\src\assets\albums\tpab.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'untitled unmastered.',
-            id: '6',
-            img: '\src\assets\albums\ntitled.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'DAMN.',
-            id: '7',
-            img: '\src\assets\albums\damn.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Black Panther: The Album',
-            id: '8',
-            img: '\src\assets\albums\bp.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Mr Morale & the Big Steppers',
-            id: '9',
-            img: '\src\assets\albums\mmatbs.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'GNX',
-            id: '10',
-            img: '\src\assets\albums\gnx.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
-          {
-            name: 'Not Like Us',
-            id: '11',
-            img: '\src\assets\albums\nls.jpg',
-            desc: '',
-            stars: 0,
-            review: '',
-          },
+    name: 'Overly Dedicated',
+    id: '1',
+    img: overlydedicated,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Section.80',
+    id: '2',
+    img: section80,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Good Kid, M.A.A.D City',
+    id: '3',
+    img: gkmc,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Good Kid, M.A.A.D City (Deluxe)',
+    id: '4',
+    img: gkmcd,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'To Pimp a Butterfly',
+    id: '5',
+    img: tpab,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'untitled unmastered.',
+    id: '6',
+    img: uu,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'DAMN.',
+    id: '7',
+    img: damn,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Black Panther: The Album',
+    id: '8',
+    img: bp,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Mr Morale & the Big Steppers',
+    id: '9',
+    img: mmatbs,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'GNX',
+    id: '10',
+    img: gnx,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
+  {
+    name: 'Not Like Us',
+    id: '11',
+    img: nls,
+    desc: '',
+    stars: 0,
+    review: '',
+  },
         ].sort((a, b) => a.id - b.id),
   )
 
